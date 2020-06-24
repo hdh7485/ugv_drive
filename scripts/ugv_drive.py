cd80a7bfd383097fceeedda65885251036f68722
Donghee Han <hdh7485@kaist.ac.kr>
2020-06-24 23:58:26 +0900

    Fixed unit bug

--- scripts/ugv_drive.py
+++ scripts/ugv_drive.py
@@ -65,7 +65,7 @@ class ODriveController:
 
     def state_motor_calibration(self, axis):
         axis.requested_state = AXIS_STATE_MOTOR_CALIBRATION
-        rospy.loginfo(axis.motor)
+        #rospy.loginfo(axis.motor)
         if axis.motor.error == 0:
             axis.motor.config.pre_calibrated = True
             self.odrive.save_configuration()
@@ -74,20 +74,25 @@ class ODriveController:
 
     def state_encoder_offset_calibration(self, axis):
         axis.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
-        rospy.loginfo(axis.encoder)
+        #rospy.loginfo(axis.encoder)
         if axis.encoder.error == 0:
             axis.encoder.config.pre_calibrated = True
             self.odrive.save_configuration()
         else:
             rospy.logerr("{} state encoder offset calibration failed".format(axis))
 
-    def rotate_rpm_axis0(self, rad_per_sec):
-        count_per_sec = rad_per_sec * 2 * math.pi / 60 * 15
+    def rotate_rad_s_axis0(self, rad_per_sec):
+        #count_per_sec = rad_per_sec * 2 * math.pi / 60 * 15
+        #count_per_sec = rad_per_sec * 90 / (2*math.pi)
+        count_per_sec = rad_per_sec * 45 / math.pi
         self.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis0.controller.vel_setpoint = count_per_sec
+        rospy.loginfo(count_per_sec)
     
-    def rotate_rpm_axis1(self, rad_per_sec):
-        count_per_sec = rad_per_sec * 2 * math.pi / 60 * 15
+    def rotate_rad_s_axis1(self, rad_per_sec):
+        #count_per_sec = rad_per_sec * 2 * math.pi / 60 * 15
+        #count_per_sec = rad_per_sec * 90 / (2*math.pi)
+        count_per_sec = rad_per_sec * 45 / math.pi
         self.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
         self.axis1.controller.vel_setpoint = count_per_sec
 
@@ -97,15 +102,34 @@ class ODriveController:
     def request_idle_state_axis1(self):
         self.axis1.requested_state = AXIS_STATE_IDLE
 
+    def vel_estimate_axis0(self):
+        return self.axis0.encoder.vel_estimate 
+
+    def vel_estimate_axis1(self):
+        return self.axis1.encoder.vel_estimate
+
 class UGVDriver:
     def __init__(self):
+        self.odom_timer = 0.01
+        self.cpr = 90
         rospy.init_node('ugv_drive')
         self.front_odrive = ODriveController(timeout=30, serial_number="20803592524B")
         self.back_odrive  = ODriveController(timeout=30, serial_number="20583592524B")
         rospy.Subscriber('cmd_vel', Twist, self.twist_callback, queue_size=1)
-        rospy.Subscriber('/dynamixel_workbench/dynamixel_state', DynamixelStateList, self.dynamixel_callback, queue_size=1)
+        rospy.Subscriber('/dynamixel_workbench/dynamixel_state', 
+                         DynamixelStateList, self.dynamixel_callback, queue_size=1)
+        rospy.Timer(rospy.Duration(self.odom_timer), self.odom_calculator)
         self.turn_motor = rospy.ServiceProxy('dynamixel_workbench/dynamixel_command', DynamixelCommand)
-        self.current_steer_radian = [0, 0, 0, 0]
+        self.current_steer_radian = [0, 0, 0, 0]  #radian
+        self.current_wheel_speed = [0, 0, 0, 0]   #radian/sec
+
+    def odom_calculator(self, event=None):
+        wheel_speed_1 = self.front_odrive.vel_estimate_axis0() * math.pi*2/self.cpr
+        wheel_speed_2 = self.front_odrive.vel_estimate_axis1()
+        wheel_speed_3 = self.back_odrive.vel_estimate_axis0()
+        wheel_speed_4 = self.back_odrive.vel_estimate_axis1()
+        #rospy.loginfo("wh1:{} wh2:{} wh3:{} wh:{}".format(wheel_speed_1, wheel_speed_2, 
+                                                          #wheel_speed_3, wheel_speed_4))
 
     def twist_callback(self, twist_callback_data):
         # rospy.loginfo(twist_callback_data)
@@ -114,10 +138,10 @@ class UGVDriver:
         result1 = self.turn_dynamixel(2, steer_angle[1]) # BL
         result2 = self.turn_dynamixel(3, steer_angle[0]) # BR
         result3 = self.turn_dynamixel(4, steer_angle[3]) # FR
-        self.front_odrive.rotate_rpm_axis1(wheel_speed[2]) # FL
-        self.back_odrive.rotate_rpm_axis0(wheel_speed[1]) # BL
-        self.back_odrive.rotate_rpm_axis1(wheel_speed[0]) # BR
-        self.front_odrive.rotate_rpm_axis0(wheel_speed[3]) # FR
+        self.front_odrive.rotate_rad_s_axis1(wheel_speed[2]) # FL
+        self.back_odrive.rotate_rad_s_axis0(wheel_speed[1]) # BL
+        self.back_odrive.rotate_rad_s_axis1(wheel_speed[0]) # BR
+        self.front_odrive.rotate_rad_s_axis0(wheel_speed[3]) # FR
 
     def normalize_2pi(self, radian):
         normalized_radian = (radian + 2 * math.pi) % (2 * math.pi)
@@ -133,7 +157,7 @@ class UGVDriver:
         for motor in dynamixel_data.dynamixel_state:
             #rospy.loginfo(int(motor.id)-1)
             self.current_steer_radian[int(motor.id)-1] = self.value2radian(motor.present_position)
-        #rospy.loginfo(self.current_steer_radian)
+        #rospy.loginfo("steer_radian:{}".format(self.current_steer_radian))
 
     def value2radian(self, value):
         radian = float(value) / -501923.0 * math.pi
@@ -159,21 +183,21 @@ class UGVDriver:
             print "Service call failed: %s"%e
 
     def calculate_joint_kinetics(self, twist_data):
-        wheel_position = [{'x':0.6, 'y':0.4}, {'x':0.6, 'y':-0.4}, {'x':-0.6, 'y':-0.4}, {'x':-0.6, 'y':0.4}]
-        wheel_radius = 0.0127
-        steer_angle = [0.0, 0.0, 0.0, 0.0]
+        wheel_position = [{'x':0.6, 'y':0.4}, {'x':0.6, 'y':-0.4}, {'x':-0.6, 'y':-0.4}, {'x':-0.6, 'y':0.4}] #m
+        wheel_radius = 0.127 #m
+        steer_angle = [0.0, 0.0, 0.0, 0.0] #rad
         steer_speed = [0.0, 0.0, 0.0, 0.0]
-        wheel_speed = [0.0, 0.0, 0.0, 0.0]
+        wheel_speed = [0.0, 0.0, 0.0, 0.0] #rad/s
         wheel_distance = [0.0, 0.0, 0.0, 0.0]
         wheel_angle_rad = [0.0, 0.0, 0.0, 0.0]
         target_steer_1 = [0.0, 0.0, 0.0, 0.0]
         target_steer_2 = [0.0, 0.0, 0.0, 0.0]
-        target_speed_1 = [0.0, 0.0, 0.0, 0.0]
+        target_speed_1 = [0.0, 0.0, 0.0, 0.0] #rad/s
         target_speed_2 = [0.0, 0.0, 0.0, 0.0]
 
-        x_vel = twist_data.linear.x
+        x_vel = twist_data.linear.x #m/s
         y_vel = twist_data.linear.y
-        angular_vel = twist_data.angular.z
+        angular_vel = twist_data.angular.z #rad/s
 
         if abs(x_vel) < 0.001 and abs(y_vel) < 0.001 and abs(angular_vel) < 0.001:
             pass
@@ -199,8 +223,9 @@ class UGVDriver:
                 # now adapt to direction (forward/backward) of wheel
                 target_speed_2[i] = -target_speed_1[i]
             wheel_speed = target_speed_1
+            rospy.loginfo(wheel_speed)
             steer_angle = target_steer_1
-        rospy.loginfo(abs(steer_angle[2] - self.current_steer_radian[0]))
+        #rospy.loginfo(abs(steer_angle[2] - self.current_steer_radian[0]))
         if abs(steer_angle[2] - self.current_steer_radian[0]) > math.pi/2+0.02:
             steer_angle[2] += math.pi
             normalized_value = self.normalize_pi(steer_angle[2])
@@ -225,8 +250,8 @@ class UGVDriver:
             if normalized_value > -math.pi and  normalized_value < math.pi:
                 steer_angle[3] = normalized_value
                 wheel_speed[3] *= -1
-        rospy.loginfo(wheel_speed)
-        rospy.loginfo(target_speed_1)
+        #rospy.loginfo(wheel_speed)
+        #rospy.loginfo(target_speed_1)
 
         return steer_angle, steer_speed, wheel_speed
 
